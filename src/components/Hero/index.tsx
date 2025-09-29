aad4e65ffaa7fcc4b988c9290f1319f542b185b6
Seitek2002 <Seitek2002@users.noreply.github.com>
2025-09-29 16:17:54 +0600

    test

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -91,7 +91,7 @@ const Hero = () => {
   };
 
   return (
-    !!orders && (<section className='hero'>
+    orders?.length && (<section className='hero'>
       <Swiper
         pagination={{ dynamicBullets: true }}
         modules={[Pagination]}
